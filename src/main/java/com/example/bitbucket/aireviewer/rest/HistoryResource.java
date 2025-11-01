68e4ea0c33f52a6cf6e3c89261ab2103e58a4ce4
TeknolojikPanda <dogaucak@gmail.com>
2025-11-01 20:22:40 +0300

    chore: tidy imports after observability updates

--- src/main/java/com/example/bitbucket/aireviewer/rest/HistoryResource.java
+++ src/main/java/com/example/bitbucket/aireviewer/rest/HistoryResource.java
@@ -20,7 +20,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
--- src/main/java/com/example/bitbucket/aireviewer/service/ConfigurationValidationException.java
+++ src/main/java/com/example/bitbucket/aireviewer/service/ConfigurationValidationException.java
@@ -4,7 +4,6 @@ import javax.annotation.Nonnull;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Exception thrown when configuration validation fails.
